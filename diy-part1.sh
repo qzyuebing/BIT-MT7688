8dfdbf8c4b7b61a79a9b1fda9a66e394c59c19a5
qzyuebing <54806519+qzyuebing@users.noreply.github.com>
2022-12-04 09:04:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+ sed -i '$a src-git smpackage https://github.com/kenzok78/small-package' feeds.conf.default
+#sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.conf.default
+#sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.default
-sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.conf.default
-sed -i '$a src-git mmdvm https://github.com/qzyuebing/mmdvm-openwrt' feeds.conf.default
+sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.default
+sed -i '$a src-git mmdvm https://github.com/sgyuebing/mmdvm-openwrt' feeds.conf.default
